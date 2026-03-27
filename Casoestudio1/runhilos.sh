454e133f23241265201aeaafbcaf4db7c4d18c1c
KevinCastro25 <kevin.castro1@utp.edu.co>
2026-03-26 19:45:10 -0500

    Ajustes caso de estudio 1

--- Casoestudio1/runhilos.sh
+++ Casoestudio1/runhilos.sh
@@ -4,7 +4,7 @@
 echo "Ejecutando pruebas con 12 hilos..."
 for ((j=1; j<=10; j++)); do
     for i in 800 1000 2000 4000 6000 8000; do
-        ./matriceshilos "$i" 12 >> timehilos12.doc
+        ./matriceshilos "$i" 12 >> timehilos12aux.doc
     done
 done
 
